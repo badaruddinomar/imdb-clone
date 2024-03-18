5173745d20bcd31e495c67f3cd37b3da8d59b2f5
badaruddinomar <badaruddinomar2403@gmail.com>
2024-03-18 21:46:14 +0600

    about page added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="px-[20px] md:px-[40px] lg:px-[60px] py-5">
+      <h1 className="text-2xl">About Us</h1>
+      <p className="leading-[1.6] my-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        maxime dicta suscipit ipsa cupiditate eum delectus nobis beatae laborum
+        quia. Dolores molestiae velit id in officiis quisquam porro ut cumque
+        dolore! Aliquid, illo. Repudiandae rem exercitationem facilis, quos
+        distinctio assumenda dolorum unde et nostrum, recusandae, quod veritatis
+        deserunt amet? Ad. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Voluptates veritatis officiis autem quidem commodi et est qui
+        facere repellat, vero molestiae natus deserunt possimus eaque quos,
+        illum quas, dolorum odit!
+      </p>
+      <p className="leading-[1.6] my-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        maxime dicta suscipit ipsa cupiditate eum delectus nobis beatae laborum
+        quia. Dolores molestiae velit id in officiis quisquam porro ut cumque
+        dolore! Aliquid, illo. Repudiandae rem exercitationem facilis, quos
+        distinctio assumenda dolorum unde et nostrum, recusandae, quod veritatis
+        deserunt amet? Ad. Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Qui doloribus alias, consequatur iusto aspernatur at quam rem
+        tenetur recusandae nisi impedit eius exercitationem laboriosam ullam
+        ipsa modi quo et esse!
+      </p>
+      <p className="leading-[1.6] my-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        maxime dicta suscipit ipsa cupiditate eum delectus nobis beatae laborum
+        quia. Dolores molestiae velit id in officiis quisquam porro ut cumque
+        dolore! Aliquid, illo. Repudiandae rem exercitationem facilis, quos
+        distinctio assumenda dolorum unde et nostrum, recusandae, quod veritatis
+        deserunt amet? Ad. Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Aliquid distinctio, voluptates similique sit rerum quidem, itaque
+        tenetur voluptas laborum sint facere deleniti mollitia nostrum
+        voluptatem ducimus? Assumenda ut dicta hic?
+      </p>
+    </div>
+  );
+};
+
+export default About;
