b1d7ce4809ae01ae2b890efbad64288a0ae52b58
badaruddinomar <badaruddinomar2403@gmail.com>
2024-03-18 22:18:03 +0600

    navbar component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,35 @@
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+
+  return (
+    <nav className="w-full h-[60px] dark:bg-gray-400 bg-amber-500 flex items-center justify-center">
+      <Link
+        href="/?genre=trendingMovie"
+        className={`text-[16px] text-white mx-5 ${
+          genre === "trendingMovie"
+            ? "border-b-2 border-amber-500 dark:border-white"
+            : ""
+        }`}
+      >
+        Trending
+      </Link>
+      <Link
+        href="/?genre=topRated"
+        className={`text-[16px] text-white mx-5 ${
+          genre === "topRated"
+            ? "border-b-2 border-amber-500 dark:border-white"
+            : ""
+        }`}
+      >
+        Top Rated
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
